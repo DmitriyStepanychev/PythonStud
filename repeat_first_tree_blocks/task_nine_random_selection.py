f032cd5190d5949b6ef34b5125d82cc21870c03b
Dmitrii Stepanychev <stepanychevdmitriy@gmail.com>
2025-11-16 18:37:39 +0300

    Add nine task in repeat block

--- repeat_first_tree_blocks/task_nine_random_selection.py
+++ repeat_first_tree_blocks/task_nine_random_selection.py
@@ -2,3 +2,16 @@
 # выведи случайного победителя.
 # ⚙️ Для продвинутого уровня — попроси ввести список имён вручную.
 
+import random
+
+name_list = []
+
+while len(name_list) < 5:
+    add_name = input("Enter at least five names: ")
+    if add_name not in name_list:
+        name_list.append(add_name)
+    else:
+        print('Name already in use')
+
+print(f"Name list: {name_list}")
+print(f"Random winner is {random.choice(name_list)}")
